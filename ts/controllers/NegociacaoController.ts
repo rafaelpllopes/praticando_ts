d96956f784fe5c255cfcef6505f20e65985a7bc9
rafaelpllopes <rafael.pllopes@gmail.com>
2018-09-09 17:24:02 -0300

    Evitando carregar negociacoes duplicadas com filter e some.

--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -67,9 +67,15 @@ export class NegociacaoController {
                     }
                 }
             )
-            .then(negociacoes => {
-                negociacoes.forEach(negociacao =>
-                    this._negociacoes.adiciona(negociacao))
+            .then(negociacoesParaImportar => {
+                const negociacoesJaImportadas = this._negociacoes.paraArray();
+
+                negociacoesParaImportar
+                    .filter(negociacao =>
+                        !negociacoesJaImportadas.some(jaImportada =>
+                            negociacao.ehIgual(jaImportada)))
+                    .forEach(negociacao =>
+                        this._negociacoes.adiciona(negociacao))
                 this._negociacoesView.update(this._negociacoes);
             });
 
--- ts/models/Negociacao.ts
+++ ts/models/Negociacao.ts
@@ -1,6 +1,7 @@
 import { Imprimivel } from "./Imprimivel";
+import { Igualavel } from "./Igualavel";
 
-export class Negociacao implements Imprimivel {
+export class Negociacao implements Imprimivel, Igualavel<Negociacao> {
 
     constructor(
         readonly data: Date,
@@ -19,5 +20,11 @@ export class Negociacao implements Imprimivel {
             Valor: ${this.valor}, 
             Volume: ${this.volume}`
         );
+    }
+
+    ehIgual(negociacao: Negociacao): boolean {
+        return this.data.getDate() == negociacao.data.getDate()
+            && this.data.getMonth() == negociacao.data.getMonth()
+            && this.data.getFullYear() == negociacao.data.getFullYear();
     }
 }
